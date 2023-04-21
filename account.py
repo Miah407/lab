0fcb21e2b04e2c2f5fd7e198061ddd3090697582
athen <miahmason41@gmail.com>
2023-04-21 17:41:44 -0500

    first file

--- account.py
+++ account.py
@@ -4,14 +4,14 @@ class Account:
         self.account_name = name
         self.account_balance = 0
 
-    def deposit(self, amount) -> bool:
+    def deposit(self, amount: float) -> bool:
         if amount > 0:
             self.account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount) -> bool:
+    def withdraw(self, amount: float) -> bool:
         if 0 < amount < self.account_balance:
             self.account_balance -= amount
             return True
