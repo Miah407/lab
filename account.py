5ea47d908f424774f44729ede5fa24a47b5aa7cc
athen <miahmason41@gmail.com>
2023-04-21 19:08:09 -0500

    added second file

--- account.py
+++ account.py
@@ -1,16 +1,17 @@
 class Account:
-    def __init__(self, name):
+
+    def __init__(self, name: str) -> None:
         self.account_name = name
         self.account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
         if amount > 0:
             self.account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
         if 0 < amount < self.account_balance:
             self.account_balance -= amount
             return True
@@ -22,3 +23,4 @@ class Account:
 
     def get_name(self):
         return self.account_name
+
